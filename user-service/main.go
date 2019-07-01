2276038ee85f3646a37819753317fde3d5fe876b
shuza <shuza.sa@gmail.com>
2019-07-01 17:40:01 +0600

    rewrite to micro framework

--- user-service/main.go
+++ user-service/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
+	"github.com/micro/go-micro"
 	"github.com/shuza/porter/user-service/db"
 	pb "github.com/shuza/porter/user-service/proto"
 	"github.com/shuza/porter/user-service/service"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-	"log"
-	"net"
-	"os"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -19,24 +16,17 @@ func main() {
 	defer dbConn.Close()
 	dbConn.AutoMigrate(&pb.User{})
 	repo := &db.UserRepository{dbConn}
-	tokenService := &service.TokenService{repo}
+	tokenService := service.TokenService{repo}
 
-	port := os.Getenv("PORT")
+	srv := micro.NewService(
+		micro.Name("porter.auth"),
+	)
+	srv.Init()
 
-	//	setup gRPC server
-	listen, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalln("failed to listen  :  ", err)
+	pb.RegisterUserServiceHandler(srv.Server(), &service.UserService{repo, &tokenService})
+
+	log.Println("Auth service is running...")
+	if err := srv.Run(); err != nil {
+		panic(err)
 	}
-
-	s := grpc.NewServer()
-	userService := service.UserService{repo, tokenService}
-	pb.RegisterUserServiceServer(s, &userService)
-	reflection.Register(s)
-
-	log.Println("Running on port :  ", port)
-	if err := s.Serve(listen); err != nil {
-		log.Fatalln("failed to server  :  ", err)
-	}
-
 }
--- user-service/service/user.go
+++ user-service/service/user.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/shuza/porter/user-service/db"
 	pb "github.com/shuza/porter/user-service/proto"
 	"golang.org/x/crypto/bcrypt"
@@ -14,68 +15,79 @@ type UserService struct {
 	tokenService Authable
 }
 
-func (s *UserService) Create(ctx context.Context, req *pb.User) (*pb.Response, error) {
+func (s *UserService) Create(ctx context.Context, req *pb.User, resp *pb.Response) error {
 	hashedPass, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	req.Password = string(hashedPass)
 	if err := s.repo.Create(req); err != nil {
-		return nil, err
+		return errors.New(fmt.Sprintf("Create user DB Error :  %v\n", err))
 	}
+	resp.User = req
+	token, err := s.tokenService.Encode(req)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Create user token create  Error  :   %v\n", err))
+	}
+	resp.User = req
+	resp.Token = &pb.Token{Token: token}
 
-	return &pb.Response{User: req}, nil
+	return nil
 }
 
-func (s *UserService) Get(ctx context.Context, req *pb.User) (*pb.Response, error) {
+func (s *UserService) Get(ctx context.Context, req *pb.User, resp *pb.Response) error {
 	user, err := s.repo.Get(req.Id)
 	if err != nil {
-		return nil, err
+		return err
 	}
+	resp.User = user
 
-	return &pb.Response{User: user}, nil
+	return nil
 }
 
-func (s *UserService) GetAll(ctx context.Context, req *pb.Empty) (*pb.Response, error) {
+func (s *UserService) GetAll(ctx context.Context, req *pb.Empty, resp *pb.Response) error {
 	users, err := s.repo.GetAll()
 	if err != nil {
-		return nil, err
+		return err
 	}
+	resp.Users = users
 
-	return &pb.Response{Users: users}, nil
+	return nil
 }
 
-func (s *UserService) Auth(ctx context.Context, req *pb.User) (*pb.Token, error) {
+func (s *UserService) Auth(ctx context.Context, req *pb.User, resp *pb.Token) error {
 	log.Printf("Logging in with :  %v  %v\n", req.Email, req.Password)
 	user, err := s.repo.GetByEmail(req.Email)
 	log.Println(user)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	//	Compare our given password with the hashed password stored in database
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
-		return nil, err
+		return err
 	}
 
 	token, err := s.tokenService.Encode(user)
 	if err != nil {
-		return nil, err
+		return err
 	}
+	resp.Token = token
 
-	return &pb.Token{Token: token}, nil
+	return nil
 }
 
-func (s *UserService) ValidateToken(ctx context.Context, req *pb.Token) (*pb.Token, error) {
+func (s *UserService) ValidateToken(ctx context.Context, req *pb.Token, resp *pb.Token) error {
 	claim, err := s.tokenService.Decode(req.Token)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	if claim.User.Id == "" {
-		return nil, errors.New("invalid user")
+		return errors.New("invalid user")
 	}
+	resp.Valid = true
 
-	return &pb.Token{Valid: true}, nil
+	return nil
 }
